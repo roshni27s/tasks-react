3052bb4ae2372f4ccb0f789bf25333604fd15f1a
Roshni Singh <roshni.rx@gmail.com>
2022-12-09 23:25:29 +0530

    event handlers

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import Card from './Card';
+import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-    
+    const clickHandler = () => {
+      console.log('clicked');
+    }
   return (
     <Card className='expense-item'>
         <ExpenseDate date={props.date}/>
@@ -12,6 +14,7 @@ const ExpenseItem = (props) => {
             <h2>{props.title}</h2>
             <div className='expense-item__price'>${props.amount}</div>
         </div>
+        <button onClick={clickHandler}>Change title</button>
     </Card>
   )
 }
